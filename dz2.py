185c6d174ca05631f88119e1b21d7a66c4c35768
WondarLock <ironant11@gmail.com>
2020-05-05 02:13:03 +0300

    добавил дз2

--- dz2.py
+++ dz2.py
@@ -4,3 +4,25 @@
 # cars.sort()
 # cars.reverse()
 # print(cars)
+import random
+
+luckyNumber = random.randint(1, 100)
+print(luckyNumber)
+difficulty = int(input('Выбери количество попыток: '))
+
+attempts = 1
+userNumber = int(input('И ваша цифра: '))
+
+while attempts != difficulty:
+
+    if luckyNumber == (userNumber):
+        print('GGWP')
+        break
+    elif luckyNumber > int(userNumber):
+        print('Бери больше')
+    else:
+        print('Бери меньше')
+    userNumber =int(input('И ваша цифра: '))
+    attempts += 1
+if userNumber != luckyNumber:
+    print('Ты проиграл, число было:', luckyNumber, '\nПопробуй еще')
